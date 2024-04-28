ac27eb1514703044190a5aeb7131d0cfd9bcd6e4
fabiosimplis <fabio.simplis@gmail.com>
2024-04-28 17:21:24 -0300

    :construction: Defining HTML Structure from Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 import './styles.css'
 
+import padlock from '../../assets/padlock.png'
+import logo from '../../assets/fj.jpg'
+
 export default function Login({children}){
     return (
-        <h1>Welcome</h1> 
+        <div className="login-container">
+            <section className="form">
+                <img src={logo} alt="FJ Logo"/>
+                <form action="">
+                    <h1>Access your Accout</h1>
+                    <input placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button type="submit"/>
+                </form>
+
+            </section>
+            
+            <img src={padlock} alt="Login" />
+
+        </div>
     )
 }
