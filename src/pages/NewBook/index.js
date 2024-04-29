84f34ea556feb90b2c7d14a954ba76deee3efa82
fabiosimplis <fabio.simplis@gmail.com>
2024-04-29 20:29:22 -0300

    :lipstick: Styling the Book Registration Screen

--- src/pages/NewBook/index.js
+++ src/pages/NewBook/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Link} from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 
-//import './styles.css';
+import './styles.css';
 import logo from '../../assets/fj.jpg'
 
 
