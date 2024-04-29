5016e17a3a46c70c5d8ed5422cf145d3e0efdc17
fabiosimplis <fabio.simplis@gmail.com>
2024-04-29 16:11:55 -0300

    :construction: Styling the Books Screen Listing

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -36,8 +36,80 @@ export default function Book(){
                         <FiTrash2 size={20} color="#251FC5"/>
                     </button>
                 </li>
-            </ul>
 
+                <li>
+                    <strong>Title: </strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author: </strong>
+                    <p>Nigel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date: </strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Title: </strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author: </strong>
+                    <p>Nigel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date: </strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Title: </strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author: </strong>
+                    <p>Nigel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date: </strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+
+                <li>
+                    <strong>Title: </strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author: </strong>
+                    <p>Nigel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date: </strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+            </ul>
+            
         </div>
     );
 }
